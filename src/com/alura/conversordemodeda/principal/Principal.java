5222996b79af04dd4a23b4f1ab954a3d76cbe2be
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2024-09-28 17:48:30 -0400

    Control over decimal I added 5 decimal

--- src/com/alura/conversordemodeda/principal/Principal.java
+++ src/com/alura/conversordemodeda/principal/Principal.java
@@ -3,9 +3,13 @@ package com.alura.conversordemodeda.principal;
 import com.alura.conversordemodeda.modelos.CurrencyConverter;
 import com.alura.conversordemodeda.modelos.ExchangeRateService;
 
+import java.text.DecimalFormat;  // Importa DecimalFormat
 import java.util.Scanner;
 
 public class Principal {
+    // Crea una instancia de DecimalFormat para limitar los decimales a 5
+    private static final DecimalFormat decimalFormat = new DecimalFormat("0.00000");
+
     public static void main(String[] args) {
         ExchangeRateService exchangeRateService = new ExchangeRateService();
         CurrencyConverter converter = new CurrencyConverter(exchangeRateService);
@@ -16,7 +20,7 @@ public class Principal {
             System.out.println("**********************");
             System.out.println("\nSelecciona la conversión:");
             System.out.println("1. Dólar ==> Peso Argentino");
-            System.out.println("2. Peso Argentino ==> Dóllar");
+            System.out.println("2. Peso Argentino ==> Dólar");
             System.out.println("3. Dólar ==> Real Brasileño");
             System.out.println("4. Real Brasileño ==> Dólar");
             System.out.println("5. Dólar ==> Peso Colombiano");
@@ -36,56 +40,57 @@ public class Principal {
                         System.out.print("Ingresa la cantidad en dólares: ");
                         double dollarsToArs = scanner.nextDouble();
                         double resultArs = converter.convertDollarsToPesosArgentinos(dollarsToArs);
-                        System.out.println("Equivalente en pesos argentinos: " + resultArs);
+                        // Formatear el resultado a 3 decimales
+                        System.out.println("Equivalente en pesos argentinos: " + decimalFormat.format(resultArs));
                         pressEnterToContinue();
                         break;
                     case 2:
                         System.out.print("Ingresa la cantidad en pesos argentinos: ");
                         double arsToDollars = scanner.nextDouble();
-                        double resultMx = converter.convertPesosArgentinosToDollars(arsToDollars);
-                        System.out.println("Equivalente en dólares: " + resultMx);
+                        double resultDollars = converter.convertPesosArgentinosToDollars(arsToDollars);
+                        System.out.println("Equivalente en dólares: " + decimalFormat.format(resultDollars));
                         pressEnterToContinue();
                         break;
                     case 3:
                         System.out.print("Ingresa la cantidad en dólares: ");
                         double dollarsToBrl = scanner.nextDouble();
                         double resultBrl = converter.convertDollarsToRealesBrasilenos(dollarsToBrl);
-                        System.out.println("Equivalente en reales brasileños: " + resultBrl);
+                        System.out.println("Equivalente en reales brasileños: " + decimalFormat.format(resultBrl));
                         pressEnterToContinue();
                         break;
                     case 4:
                         System.out.print("Ingresa la cantidad en reales brasileños: ");
                         double brlToUsd = scanner.nextDouble();
                         double resultUsdFromBrl = converter.convertRealesBrasilenosToDollars(brlToUsd);
-                        System.out.println("Equivalente en dólares: " + resultUsdFromBrl);
+                        System.out.println("Equivalente en dólares: " + decimalFormat.format(resultUsdFromBrl));
                         pressEnterToContinue();
                         break;
                     case 5:
                         System.out.print("Ingresa la cantidad en dólares: ");
                         double dollarsToCop = scanner.nextDouble();
                         double resultCop = converter.convertDollarsToPesosColombianos(dollarsToCop);
-                        System.out.println("Equivalente en pesos colombianos: " + resultCop);
+                        System.out.println("Equivalente en pesos colombianos: " + decimalFormat.format(resultCop));
                         pressEnterToContinue();
                         break;
                     case 6:
                         System.out.print("Ingresa la cantidad en pesos colombianos: ");
                         double copToUsd = scanner.nextDouble();
                         double resultUsdFromCop = converter.convertPesosColombianosToDollars(copToUsd);
-                        System.out.println("Equivalente en dólares: " + resultUsdFromCop);
+                        System.out.println("Equivalente en dólares: " + decimalFormat.format(resultUsdFromCop));
                         pressEnterToContinue();
                         break;
                     case 7:
-                        System.out.print("Ingresa la cantidad en colones Costa Rica: ");
-                        double crcToUsd = scanner.nextDouble();
-                        double resultCrc = converter.convertColonesCostaRicatoDollars(crcToUsd);
-                        System.out.println("Equivalente en dólares: " + resultCrc +" [CRC]");
+                        System.out.print("Ingresa la cantidad en dólares: ");
+                        double usdToCrc = scanner.nextDouble();
+                        double resultCrcFromUsd = converter.converDollarsToColonesCostaRica(usdToCrc);
+                        System.out.println("Equivalente en colones costarricenses: " + decimalFormat.format(resultCrcFromUsd));
                         pressEnterToContinue();
                         break;
                     case 8:
-                        System.out.print("Ingresa la cantidad en dolares: ");
-                        double usdToCrc = scanner.nextDouble();
-                        double resultCrcFromUSA = converter.converDollarsToColonesCostaRica(usdToCrc);
-                        System.out.println("Equivalente en dólares: " + resultCrcFromUSA);
+                        System.out.print("Ingresa la cantidad en colones costarricenses: ");
+                        double crcToUsd = scanner.nextDouble();
+                        double resultUsdFromCrc = converter.convertColonesCostaRicatoDollars(crcToUsd);
+                        System.out.println("Equivalente en dólares: " + decimalFormat.format(resultUsdFromCrc));
                         pressEnterToContinue();
                         break;
                     case 10:
